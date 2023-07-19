8985fbd5507ad000ed11fbc067d8ce56dea9bf0d
Hon07 <hangulo14@gmail.com>
2023-07-19 03:40:50 +0000

    backend models and some routes

--- src/api/models.py
+++ src/api/models.py
@@ -17,3 +17,165 @@ class User(db.Model):
             "email": self.email,
             # do not serialize the password, its a security breach
         }
+
+
+class People(db.Model):
+    __tablename__ = "people"
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(50), nullable = False)
+    gender = db.Column(db.String(50), nullable = False)
+    height = db.Column(db.Integer, nullable = False)
+    skin_color = db.Column(db.String(50), nullable = False)
+    eyes_color = db.Column(db.String(50), nullable = False)
+    birth_year = db.Column(db.String(50), nullable = False)
+    def __repr__(self):
+        return f'<People {self.id}>'
+    def serialize(self):
+        return {
+            "id":self.id,
+            "name": self.name,
+            "gender": self.gender,
+            "height": self.height,
+            "skin_color": self.skin_color,
+            "eyes_color": self.eyes_color,
+            "birth_year": self.birth_year,
+        }
+
+
+
+class Planets(db.Model):
+    __tablename__= "planets"
+    id = db.Column(db.Integer, primary_key= True)
+    name = db.Column(db.String(50), nullable = False)
+    diameter = db.Column(db.Integer, nullable = False)
+    gravity = db.Column(db.String(50), nullable = False)
+    population = db.Column(db.Integer, nullable = False)
+    terrain = db.Column(db.String(50), nullable = False)
+    climate = db.Column(db.String(50), nullable = False)
+    def __repr__(self):
+        return f'<Planets {self.id}>'
+        def serialize(self):
+            return {
+            "id": self.id,
+            "name": self.name,
+            "diameter": self.diameter,
+            "gravity": self.gravity,
+            "population": self.population,
+            "terrain": self.terrain,
+            "climate": self.climate,
+            }
+
+
+class Vehicles(db.Model):
+    __tablename__= "vehicles"
+    id = db.Column(db.Integer, primary_key = True)
+    model = db.Column(db.String(50), nullable = False)
+    manufacturer = db.Column(db.String(100), nullable = False) 
+    lenght = db.Column(db.Integer, nullable = False)
+    cargo_capacity = db.Column(db.Integer, nullable = False)
+    consumables = db.Column(db.String(100), nullable = False)
+    def __repr__(self):
+        return f'<Vehicles {self.id}>'
+    def serialize(self):
+        return{
+            "id":self.id,
+            "model": self.model,
+            "manufacturer": self.manufacturer,
+            "lenght": self.lenght,
+            "cargo_capacity": self.cargo_capacity,
+            "consumables": self.consumables,
+        }
+
+class Starships(db.Model):
+    __tablename__="starships"
+    id = db.Column(db.Integer, primary_key = True)
+    model = db.Column(db.String(50), nullable = False)
+    manufacturer = db.Column(db.String(100), nullable = False)
+    lenght = db.Column(db.Integer, nullable = False)
+    cargo_capacity = db.Column(db.Integer, nullable = False)
+    consumables = db.Column(db.String(100), nullable = False)
+    max_atmosphering_speed = db.Column(db.Integer, nullable = False)
+    def __repr__(self):
+        return f'<Starships {self.id}>'
+    def serialize(self):
+        return{
+            "id":self.id,
+            "model": self.model,
+            "manufacturer": self.manufacturer,
+            "lenght": self.lenght,
+            "cargo_capacity": self.cargo_capacity,
+            "consumables": self.consumables,
+            "max_atmosphering_speed": self.max_atmosphering_speed,
+        }
+
+class Films(db.Model):
+    __tablename__="films"
+    id = db.Column(db.Integer, primary_key = True)
+    title = db.Column(db.String(50), nullable = False)
+    director = db.Column(db.String(50), nullable = False)
+    producer = db.Column(db.String(50), nullable = False)
+    release_date = db.Column(db.String(50), nullable = False)
+    episode_id = db.Column(db.Integer, nullable = False)
+    def __repr__(self):
+        return f'<Films {self.id}>'
+        def serialize(self):
+            return{
+                "id": self.id,
+            "title": self.title,
+            "director": self.director,
+            "producer": self.producer,
+            "release_date": self.release_date,
+            "episode_id": self.episode_id,
+            }
+
+class Species(db.Model):
+    __tablename__="species"
+    id = db.Column(db.Integer, primary_key = True)
+    classification = db.Column(db.String(50), nullable = False)
+    designation = db.Column(db.String(100), nullable = False)
+    languaje = db.Column(db.String(100), nullable = False)
+    skin_color = db.Column(db.String(100), nullable = False)
+    eye_color = db.Column(db.String(50), nullable = False)
+    average_lifespan = db.Column(db.String(100), nullable = False)
+    def __repr__(self):
+        return f'<Species{self.id}>'
+    def serialize(self):
+        return{
+           "id":self.id,
+            "classification": self.classification,
+            "designation": self.designation,
+            "languaje": self.languaje,
+            "skin_color": self.skin_color,
+            "eye_color": self.eye_color,
+            "average_lifespan": self.average_lifespan,  
+        }
+
+class Favorites(db.Model):
+    __tablename__="favorites"
+    id = db.Column(db.Integer, primary_key = True)
+    favorite_type = db.Column(db.String(50), nullable = False)
+    element_id = db.Column(db.Integer, nullable = False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    user = db.relationship(User)
+    def __repr__(self):
+        return f'<Favorites {self.id}>'
+    def serialize(self):
+        if self.favorite_type == "films":
+            elements = Films.query.filter(Films.id == self.element_id).first()
+        elif self.favorite_type == "people": 
+            elements = People.query.filter(People.id == self.element_id).first()
+        elif self.favorite_type == "planets": 
+            elements = Planets.query.filter(Planets.id == self.element_id).first()
+        elif self.favorite_type == "species": 
+            elements = Species.query.filter(Species.id == self.element_id).first()
+        elif self.favorite_type == "starships": 
+            elements = Starships.query.filter(Starships.id == self.element_id).first()
+        elif self.favorite_type == "vehicles": 
+            elements = Vehicles.query.filter(Vehicles.id == self.element_id).first()
+        return{
+            "id": self.id,
+            "favorite_type": self.favorite_type,
+            "user":self.user.email,
+            "user_id":self.user.id,
+            "element": elements.serialize()
+        }
--- src/api/routes.py
+++ src/api/routes.py
@@ -2,7 +2,7 @@
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
 from flask import Flask, request, jsonify, url_for, Blueprint
-from api.models import db, User
+from api.models import db, User, Planets, Vehicles, Species, Starships, Favorites
 from api.utils import generate_sitemap, APIException
 
 api = Blueprint('api', __name__)
@@ -16,3 +16,173 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
+
+
+
+# Get all users
+
+
+@api.route('/users', methods=["GET"])
+def users():
+    users = User.query.filter(user.__tablename__=="user").all()
+    all_users=[]
+    for i in range(len(users)):
+        all_users.append(users[i].serialize())
+        if len(all_users)>0:
+            return jsonify({
+                "all users":all_users
+            })
+        return jsonify({
+            "message":"there are no users"
+        })
+
+#Route to get each user
+
+@api.route("/users/<int:user>", methods=["GET"])
+def get_user(user):
+    user = User.query.filter(User.id== user).first()
+    if user != None:
+        return jsonify({
+            "user": user.serialize()
+        }), 200
+    return jsonify({
+        "message":"User not found"
+    })
+
+#Create a new user
+
+api.route("/users", methods=["POST"])
+def create_user():
+    email = request.json.get("email")
+    password = request.json.get("password")
+    is_active = request.json.get("is_active")
+    create_user = User(email = email, password = password, is_active = is_active)
+    users = User.query.filter(User.email == email).first()
+
+    if users != None:
+        return jsonify({
+            "message": "The user already exists"
+        })
+    db.session.add(create_user)
+    db.session.commit()
+    return jsonify({
+        "message":"user created succesfully"
+    }), 201
+
+
+#Update user
+
+@api.route("/users/<int:user_id>", methods=["PUT"])
+def update_user(user_id):
+    email = request.json.get("email") 
+    password = request.json.get("password")
+    is_active = request.json.get("is_active")
+    user = User.query.filter(User.id == user_id).all()
+
+    if len(user)==0:
+        return jsonify({
+            "message":"User not found"
+        }), 404
+    updated_user = user[0]
+    updated_useruser.email = email
+    updated_user.password = password
+    updated_user.is_active = is_active
+    
+    db.session.add(new_user)
+    db.session.commit()
+    return jsonify({
+        "message": "User updated succesfully"
+    })
+
+#Delete all users
+
+@api.route("/users",methods=["DELETE"])
+def delete_users():
+    users = User.query.filter(User.__tablename__ == "user").all()
+    if len(users)==0:
+        return jsonify({
+            "message": "No users were found"
+        }),404
+    for i in range(len(users)):
+        db.session.delete(users[i])
+        db.session.commit()
+
+    return jsonify({
+        "message": "users deleted succesfully"
+    }),201
+
+#Delete a user
+
+@api.route("/users<int:user>", methods=["DELETE"])
+def delete_user(user):
+    user = User.query.filter(User.id== user).first()
+    if user == None:
+        return jsonify({
+            "message":"User does not exist"
+        }),404
+    db.session.delete(user)
+    db.session.commit()
+    return jsonify({
+        "message": "User deleted succesfully"
+    }
+    ), 201
+
+
+#Get favorites
+
+@api.route("/favorites", methods=["GET"])
+def favorites():
+    favorites = Favorites.query.filter(Favorites.__tablename__ == "favorites").all()
+    all_favorites=[0]
+    for i in range(len(favorites)):
+        all_favorites.append(favorites[i].serialize())
+    if len(all_favorites)>0:
+        return jsonify({
+            "Favorites": all_favorites
+        }),200
+    return jsonify({
+        "message":"There are no favorites"
+    })
+
+
+#Favorites by user
+
+@api.route("/favorites/<int:user_param>", methods=["GET"])
+def get_favorites():
+    favorites = Favorites.query.filter(Favorites.user_id== user_param).all()
+    favorites_list= []
+    if favorites == None:
+        return jsonify({
+            "message": "No favorites were found"
+        }), 404
+    for i in range(len(favorites)):
+        if favorites[i].favorite_type == 'films':
+            favorites_film = Films.query.get(favorites[i].element_id)
+            if favorites_film != None:
+                favorites[i].serialize()["data"] = favorites_film.serialize()
+        if favorites[i].favorite_type == 'people':
+            favorites_people = People.query.get(favorites[i].element_id)
+            if favorites_people != None:
+                favorites[i].serialize()["data"] = favorites_people.serialize()
+        if favorites[i].favorite_type == 'planets':
+            favorites_planet = Planets.query.get(favorites[i].element_id)
+            if favorites_planet != None:
+                favorites[i].serialize()["data"] = favorites_planet.serialize()
+        if favorites[i].favorite_type == 'species':
+            favorites_species = Species.query.get(favorites[i].element_id)
+            if favorites_species != None:
+                favorites[i].serialize()["data"] = favorites_species.serialize()
+        if favorites[i].favorite_type == 'starships':
+            favorites_starships = Starships.query.get(favorites[i].element_id)
+            if favorites_starships != None:
+                favorites[i].serialize()["data"] = favorites_starships.serialize()
+        if favorites[i].favorite_type == 'vehicles':
+            favorites_vehicles = Vehicles.query.get(favorites[i].element_id)
+            if favorites_vehicles != None:
+                favorites[i].serialize()["data"] = favorites_vehicles.serialize()
+        favorite_list.append(favorites[i].serialize())
+
+    return jsonify({
+        "Favorites": favorites_list
+    }),200
